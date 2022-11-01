49b034ab8a7b7c687058c74ff6f1cbb7fd2a0726
sciyen <a885566885566@gmail.com>
2022-11-01 10:22:04 +0800

    Adding signal handler
    Adding signal handler to properly stop the program.

--- src/stream_extraction.py
+++ src/stream_extraction.py
@@ -1,12 +1,13 @@
 # import the necessary packages
 from __future__ import print_function
 from imutils.video.pivideostream import PiVideoStream
-from imutils.video import FPS
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import argparse
 import imutils
 import time
+import signal
+import sys
 
 import numpy as np
 import socket
@@ -15,6 +16,8 @@ import yaml
 
 import cv2
 import cv2.aruco as aruco
+
+print("[INFO] Program warming up")
 
 CAM_CONFIG_FNAME = './configs/cali_640x480.yaml'
 SERV_CONFIG_FNAME = './configs/server_config.yaml'
@@ -28,28 +31,40 @@ ap.add_argument("-d", "--display", type=int, default=-1,
 args = vars(ap.parse_args())
 
 # Loading configs
+with open(SERV_CONFIG_FNAME, 'r') as f:
+    conf_serv = yaml.safe_load(f)
+
 conf_cam = cv2.FileStorage(CAM_CONFIG_FNAME, cv2.FILE_STORAGE_READ)
 mat_coef = conf_cam.getNode("matrix_coefficients").mat()
 dist_coef = conf_cam.getNode("distortion_coefficients").mat()
 resolution = (int(conf_cam.getNode("image_width").real()), int(conf_cam.getNode("image_height").real()))
-
-with open(SERV_CONFIG_FNAME, 'r') as f:
-    conf_serv = yaml.safe_load(f)
-
+ 
 # UDP setup
 UDP_client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDP_client.connect((conf_serv['serv_addr'], conf_serv['serv_port']))
-
-# Aruco marker setting
-aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)  # Use 5x5 dictionary to find markers
-parameters = aruco.DetectorParameters_create()  # Marker detection parameters
-
-vs = PiVideoStream(resolution).start()
-time.sleep(1.0)
-fps = FPS().start()
-
+ 
 def main():
-    while fps._numFrames < args["num_frames"]:
+   
+    # Aruco marker setting
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)  # Use 5x5 dictionary to find markers
+    parameters = aruco.DetectorParameters_create()  # Marker detection parameters
+
+    vs = PiVideoStream(resolution).start()
+    time.sleep(1.0)
+
+    def signal_handler(signum, frame):
+        print("[INFO] Leaving program...")
+        cv2.destroyAllWindows()
+        vs.stop()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    pre_frame_time = time.time()
+    print("[INFO] Start extracting...")
+
+    count = 0
+    obj_list = np.zeros(10)
+    while True:
         frame = vs.read()
     
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -62,9 +77,9 @@ def main():
                 distCoeff=dist_coef)
     
         if np.all(ids is not None):  # If there are markers found by detector
-            print(ids)
             tag_info = []
             for i in range(0, len(ids)):  # Iterate in markers
+                obj_list[int(ids[i, 0])] = 1
                 # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
                 rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners[i], 
                         0.1, 
@@ -88,13 +103,15 @@ def main():
             cv2.imshow("Frame", frame)
             key = cv2.waitKey(1) & 0xFF
 
-        # update the FPS counter
-        fps.update()
+        if count % 10 == 0:
+            now_frame_time = time.time()
+            fps = 10 / (now_frame_time - pre_frame_time)
+            pre_frame_time = now_frame_time
+            print("FPS: {:.2f}, obj: {}".format(fps, np.array2string(obj_list)))
+            obj_list = np.zeros(10)
 
-    # stop the timer and display FPS information
-    fps.stop()
-    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        # update the FPS counter
+        count += 1
 
     # do a bit of cleanup
     cv2.destroyAllWindows()
